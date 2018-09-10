f9dfcad1e694e1b3210bb3cd3c4fd7337bb63f82
Supavitch Sangsuwan <field1146@gmail.com>
2018-09-10 13:38:43 +0700

    del a in Order class

--- src/sample/Order.java
+++ src/sample/Order.java
@@ -6,27 +6,19 @@ public class Order {
 
     private int id;
     private String name;
-    private String a;
+
 
 
     public Order(){
         this.name = "";
         this.id = -1;
-        this.a = "B";
+
     }
 
     public Order(int id, String name) {
         this.name = name;
         this.id = id;
-        this.a = "A";
-    }
 
-    public String getA() {
-        return a;
-    }
-
-    public void setA(String a) {
-        this.a = a;
     }
 
     public String getName() {
