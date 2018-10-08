4930364bb5a504976626fad077d56327e168179d
wrsfon <wariffon@gmail.com>
2018-10-08 18:06:46 +0700

    product list: update dbclick to edit and delete Prob. edit-table doesnt update

--- src/productManagement/productListController.java
+++ src/productManagement/productListController.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -30,7 +31,7 @@ public class productListController implements Controller {
     PageController pageController;
     private TableView productListTable;
     private ObservableList<Product> products = getOrder();
-    private int index = 0;
+    private int index;
 
     public productListController(PageController pageController){
         this.pageController = pageController;
@@ -49,10 +50,10 @@ public class productListController implements Controller {
         TableColumn<Product,String> brandColumn = new TableColumn<>("BRAND");
         TableColumn<Product,Integer> amountColumn = new TableColumn<>("AMOUNT");
 
-        idColumn.setMinWidth(60);
-        nameColumn.setMinWidth(130);
-        brandColumn.setMinWidth(60);
-        amountColumn.setMinWidth(60);
+        idColumn.setMinWidth(200);
+        nameColumn.setMinWidth(200);
+        brandColumn.setMinWidth(200);
+        amountColumn.setMinWidth(199);
 
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
@@ -60,74 +61,78 @@ public class productListController implements Controller {
         amountColumn.setCellValueFactory(new PropertyValueFactory<>("quantity"));
         productListTable.getColumns().addAll(idColumn,nameColumn,brandColumn,amountColumn);
 
-        onActive();
-
-        editBt.setOnMouseClicked(new EventHandler<MouseEvent>(){
+        productListTable.setOnMouseClicked(new EventHandler<MouseEvent>(){
             @Override
-            public void handle(MouseEvent event){
-                Dialog<ButtonType> propertyDialog = new Dialog<>();
-                propertyDialog.initStyle(StageStyle.UTILITY);
-                propertyDialog.setTitle("Product Property");
+            public void handle(MouseEvent event) {
+                if (event.getClickCount() == 2) {
+                    index = productListTable.getSelectionModel().getSelectedIndex();
 
-                GridPane propertyGrid = new GridPane();
-                propertyGrid.setHgap(10);
-                propertyGrid.setVgap(20);
-                propertyGrid.setPadding(new Insets(20, 200, 10, 10));
+                    Dialog<ButtonType> propertyDialog = new Dialog<>();
+                    propertyDialog.initStyle(StageStyle.UTILITY);
+                    propertyDialog.setTitle("Product Property");
 
-                propertyGrid.add(new Label("Product ID:"), 0, 0);
-                propertyGrid.add(new Label(Integer.toString(products.get(index).getId())), 1, 0);
-                propertyGrid.add(new Label("Product Name:"), 0, 1);
-                propertyGrid.add(new Label(products.get(index).getName()), 1, 1);
-                propertyGrid.add(new Label("Product Brand:"), 0, 2);
-                propertyGrid.add(new Label(products.get(index).getBrand()), 1, 2);
-                propertyGrid.add(new Label("Product Amount:"), 0, 3);
-                propertyGrid.add(new Label(Integer.toString(products.get(index).getQuantity())), 1, 3);
-                propertyDialog.getDialogPane().setContent(propertyGrid);
+                    GridPane propertyGrid = new GridPane();
+                    propertyGrid.setHgap(10);
+                    propertyGrid.setVgap(20);
+                    propertyGrid.setPadding(new Insets(20, 250, 10, 20));
 
-                ButtonType editButtonType = new ButtonType("Edit");
-                ButtonType doneButtonType = new ButtonType("Done");
-                propertyDialog.getDialogPane().getButtonTypes().addAll(doneButtonType, editButtonType);
+                    propertyGrid.add(new Label("Product ID:"), 0, 0);
+                    propertyGrid.add(new Label(Integer.toString(products.get(index).getId())), 1, 0);
+                    propertyGrid.add(new Label("Product Name:"), 0, 1);
+                    propertyGrid.add(new Label(products.get(index).getName()), 1, 1);
+                    propertyGrid.add(new Label("Product Brand:"), 0, 2);
+                    propertyGrid.add(new Label(products.get(index).getBrand()), 1, 2);
+                    propertyGrid.add(new Label("Product Amount:"), 0, 3);
+                    propertyGrid.add(new Label(Integer.toString(products.get(index).getQuantity())), 1, 3);
+                    propertyDialog.getDialogPane().setContent(propertyGrid);
 
-                Optional<ButtonType> propertyResult = propertyDialog.showAndWait();
-                if (propertyResult.get() == editButtonType){
-                    Dialog<ButtonType> editDialog = new Dialog<>();
-                    editDialog.initStyle(StageStyle.UTILITY);
-                    editDialog.setTitle("Edit Product Property");
-                    editDialog.setHeaderText("Product ID: \t"+Integer.toString(products.get(0).getId()));
+                    ButtonType editButtonType = new ButtonType("Edit");
+                    ButtonType doneButtonType = new ButtonType("Done");
+                    propertyDialog.getDialogPane().getButtonTypes().addAll(doneButtonType, editButtonType);
 
-                    GridPane editGrid = new GridPane();
-                    editGrid.setHgap(10);
-                    editGrid.setVgap(10);
-                    editGrid.setPadding(new Insets(10, 150, 20, 10));
+                    Optional<ButtonType> propertyResult = propertyDialog.showAndWait();
+                    if (propertyResult.get() == editButtonType) {
+                        Dialog<ButtonType> editDialog = new Dialog<>();
+                        editDialog.initStyle(StageStyle.UTILITY);
+                        editDialog.setTitle("Edit Product Property");
+                        editDialog.setHeaderText("Product ID: \t" + Integer.toString(products.get(0).getId()));
 
-                    TextField name = new TextField();
-                    name.setText(products.get(index).getName());
-                    TextField brand = new TextField();
-                    brand.setText(products.get(index).getBrand());
-                    TextField amount = new TextField();
-                    amount.setText(Integer.toString(products.get(index).getQuantity()));
+                        GridPane editGrid = new GridPane();
+                        editGrid.setHgap(10);
+                        editGrid.setVgap(10);
+                        editGrid.setPadding(new Insets(10, 180, 20, 20));
 
-                    editGrid.add(new Label("Name:"), 0, 1);
-                    editGrid.add(name, 1, 1);
-                    editGrid.add(new Label("Brand:"), 0, 2);
-                    editGrid.add(brand, 1, 2);
-                    editGrid.add(new Label("Amount:"), 0, 3);
-                    editGrid.add(amount, 1, 3);
+                        TextField name = new TextField();
+                        name.setText(products.get(index).getName());
+                        TextField brand = new TextField();
+                        brand.setText(products.get(index).getBrand());
+                        TextField amount = new TextField();
+                        amount.setText(Integer.toString(products.get(index).getQuantity()));
 
-                    editDialog.getDialogPane().setContent(editGrid);
-                    editDialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK,ButtonType.CANCEL);
-                    Optional<ButtonType> editResult = editDialog.showAndWait();
-                    if (editResult.get() == ButtonType.OK){
-                        products.get(index).setName(name.getText());
-                        products.get(index).setBrand(brand.getText());
-                        products.get(index).setQuantity(amount.getText());
+                        editGrid.add(new Label("Name:"), 0, 1);
+                        editGrid.add(name, 1, 1);
+                        editGrid.add(new Label("Brand:"), 0, 2);
+                        editGrid.add(brand, 1, 2);
+                        editGrid.add(new Label("Amount:"), 0, 3);
+                        editGrid.add(amount, 1, 3);
+
+                        ButtonType deleteButtonType = new ButtonType("Delete Product");
+
+                        editDialog.getDialogPane().setContent(editGrid);
+                        editDialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL, deleteButtonType);
+                        Optional<ButtonType> editResult = editDialog.showAndWait();
+                        if (editResult.get() == ButtonType.OK) {
+                            products.get(index).setName(name.getText());
+                            products.get(index).setBrand(brand.getText());
+                            products.get(index).setQuantity(amount.getText());
+                        } else if (editResult.get() == deleteButtonType) {
+                            System.out.printf("Del");
+                            products.remove(index);
+                        }
                     }
                 }
             }
         });
-
-
-
     }
 
     @Override
@@ -136,7 +141,7 @@ public class productListController implements Controller {
     }
 
     public ObservableList<Product> getOrder(){
-        ObservableList<Product> products = FXCollections.observableArrayList();
+        products = FXCollections.observableArrayList();
         products.add(new Product(1,12,"ยาสีฟัน","ฝนฝน"));
         products.add(new Product(2,2,"dss","Por_shop2"));
         products.add(new Product(3,22,"dsd","Por_shop3"));
