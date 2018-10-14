04e41f382b77729151ad36c6bd3513fb5f543e26
Supavitch Sangsuwan <field1146@gmail.com>
2018-10-14 15:21:17 +0700

    write product list to JSON format

--- src/sample/OrderReadWrite.java
+++ src/sample/OrderReadWrite.java
@@ -1,4 +1,6 @@
 package sample;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.json.simple.*;
 
 
@@ -10,12 +12,40 @@ public class OrderReadWrite {
 
     public static void run(){
         System.out.println("OrderReadWrite online ...");
+        writeProductList();
     }
 
-    public static void writeProductList(){}
+    public static void writeProductList(){
+        ObservableList<Product> products = getProducts();
+        JSONObject obj = new JSONObject();
+        JSONObject json = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        //add product to json
+        for(Product p:products){
+            obj.put("price",p.getPrice());
+            obj.put("brand",p.getBrand());
+            obj.put("id",p.getId());
+            obj.put("name",p.getName());
+            array.add(obj.clone());
+        }
+
+        json.put("products",array);
+        System.out.println(json.toString());
+    }
 
     public static void readOrder(){}
 
     public static void writeOrderRespond(){}
+
+    public static ObservableList<Product> getProducts(){
+        ObservableList<Product> products = FXCollections.observableArrayList();
+        products.add(new Product(1,12,"ยาสีฟัน","ฝนฝน"));
+        products.add(new Product(2,2,"dss","Por_shop2"));
+        products.add(new Product(3,22,"dsd","Por_shop3"));
+        products.add(new Product(4,1,"jud","ng"));
+        products.add(new Product(5,6,"gg","pv"));
+        return products;
+    }
 
 }
