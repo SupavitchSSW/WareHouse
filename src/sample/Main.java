40eb6712d94daad032d21afac503afecde8914b9
Supavitch Sangsuwan <field1146@gmail.com>
2018-10-16 18:53:18 +0700

    create user floder

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import ordermanagement.OrderDetailController;
 import ordermanagement.OrderListController;
 import productManagement.productListController;
+import user.LoginController;
 
 import java.io.IOException;
 import java.util.Date;
@@ -17,11 +18,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        // add your fxml here
+        // >>>>>>> add your fxml here <<<<<
         Pane orderListPane = FXMLLoader.load(getClass().getResource("../ordermanagement/orderList.fxml"));
         Pane orderDetailPane = FXMLLoader.load(getClass().getResource("../ordermanagement/orderDetail.fxml"));
-
         Pane productListPane = FXMLLoader.load(getClass().getResource("../productManagement/mainPage.fxml"));
+        Pane loginPane = FXMLLoader.load(getClass().getResource("../user/login.fxml"));
+
+
 
         //create scene
         Scene scene = new Scene(productListPane,1000,600);
@@ -31,23 +34,27 @@ public class Main extends Application {
         //create pageController obj
         PageController pageController = new PageController(scene);
 
-        //crate controller obj
+
+
+        // >>>>>>> add controller class here <<<<<<
         OrderDetailController orderDetailController = new OrderDetailController(pageController);
         OrderListController orderListController = new OrderListController(pageController,orderDetailController);
-
         productListController productListController = new productListController(pageController);
+        LoginController loginController = new LoginController(pageController);
 
-        //add page to pageController
+
+
+        // >>>>>>>> add page to pageController <<<<<<<<
         pageController.addPage("orderList",orderListPane,orderListController);
         pageController.addPage("orderDetail",orderDetailPane,orderDetailController);
-
         pageController.addPage("productList",productListPane,productListController);
+        pageController.addPage("login",loginPane,loginController);
 
         OrderReadWrite.run();
 
         //start page
         primaryStage.setTitle("WareHouse");
-        pageController.active("productList");
+        pageController.active("login");
 
 
 
