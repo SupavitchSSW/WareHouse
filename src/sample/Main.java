69b315780ac6d5b64938ba20f10d71fd7ff1343c
Itiwat S <gnx18bit@hotmail.com>
2018-11-12 23:37:58 +0700

    login and signup update passwordField / Update UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -81,7 +81,7 @@ public class Main extends Application {
 
         //start page
         primaryStage.setTitle("WareHouse");
-        pageController.active("productList");
+        pageController.active("login");
 
 
     }
--- src/user/LoginController.java
+++ src/user/LoginController.java
@@ -2,10 +2,7 @@ package user;
 
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import sample.Controller;
 import sample.PageController;
@@ -25,7 +22,7 @@ public class LoginController implements Controller {
         Scene scene = pageController.getScene("login");
         Button btn = (Button) scene.lookup("#btn");
         TextField username = (TextField) scene.lookup("#id");
-        TextField password = (TextField) scene.lookup("#pw");
+        PasswordField password = (PasswordField) scene.lookup("#pw");
         Hyperlink register = (Hyperlink) scene.lookup("#register");
 
 
