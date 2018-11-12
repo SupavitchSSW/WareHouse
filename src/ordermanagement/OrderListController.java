0af7f8f549887f0e9778637c52df38c5abd07069
Supavitch Sangsuwan <field1146@gmail.com>
2018-11-12 14:02:29 +0700

    add btn in orderpage

--- src/ordermanagement/OrderListController.java
+++ src/ordermanagement/OrderListController.java
@@ -30,7 +30,8 @@ public class OrderListController implements Controller {
 
     public void initilize() {
         Scene scene = pageController.getScene("orderList");
-        Button btn = (Button) scene.lookup("#switchScene");
+        Button nextBtn = (Button) scene.lookup("#nextBtn");
+        Button backBtn = (Button) scene.lookup("#backBtn");
         TextField search_TextField = (TextField) scene.lookup("#searchBox");
 
         //search setup
@@ -51,7 +52,7 @@ public class OrderListController implements Controller {
         ownerColumn.setCellValueFactory(new PropertyValueFactory<>("owner"));
         order_table.getColumns().addAll(idColumn, nameColumn,ownerColumn);
 
-        btn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        nextBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 //get select order id
@@ -65,7 +66,12 @@ public class OrderListController implements Controller {
             }
         });
 
-
+        backBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                pageController.active("productList");
+            }
+        });
     }
 
     private void handleSearchByKey(String oldValue, String newValue) {
