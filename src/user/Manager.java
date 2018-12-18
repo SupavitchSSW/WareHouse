4d0634ba4f426c9342fd5e328aa21559562fa647
Itiwat S <gnx18bit@hotmail.com>
2018-12-18 20:38:43 +0700

    Login and Sign Up

--- src/user/Manager.java
+++ src/user/Manager.java
@@ -2,6 +2,6 @@ package user;
 
 public class Manager extends User {
     public Manager(String username, String password, String firstname, String surname, String phoneNumber) {
-        super(username, password,"Manager", firstname, surname, phoneNumber);
+        super(username, password, firstname, surname, phoneNumber,"Manager");
     }
 }
